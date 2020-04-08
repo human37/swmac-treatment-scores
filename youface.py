8b02f2fa75e74c52e90cb3ac17ffc900b2c782cd
Ammon Taylor <ammonx9@gmail.com>
2020-04-07 22:22:20 -0600

    rank functionality now works

--- youface.py
+++ youface.py
@@ -3,6 +3,9 @@ import timeago
 from flask import Flask, render_template, redirect, url_for, request, flash, make_response
 from tinydb import TinyDB
 import dbhelpers
+from geographyscore import geography_Score
+from locationscore import location_Score
+from tempscore import temperature_Score
 
 __title__ = 'SWMAC Mosquito Ranking System'
 __subtitle__ = 'Developed by students from Dixe State University'
@@ -10,10 +13,22 @@ __subtitle__ = 'Developed by students from Dixe State University'
 app = Flask('youface')
 db = TinyDB('db.json')
 
+def score(station, geography, temperature):
+    tempscore = temperature_Score(int(temperature))
+    geoscore = geography_Score(geography)
+    stationscore = location_Score(station)
+    return round(((tempscore + geoscore + stationscore) / 3),3)
+
 @app.route('/')
 def index():
+    f = open('stations.txt', 'r')
+    choices_stations = ['Select here:']
+    for line in f:
+        choices_stations.append(line.strip())
+    f.close()
+    choices_geography = ['Select here:', 'Pond', 'Creek', 'Farmland', 'Residential', 'Marshland']
     # serves the main feed page for the user
-    return render_template('feed.html')
+    return render_template('feed.html', choices = choices_geography, stations = choices_stations)
 
 @app.route('/submitdata' , methods = ['POST'])
 def submit():
@@ -21,31 +36,56 @@ def submit():
     question1 = request.form.get('q1')
     question2 = request.form.get('q2')
     question3 = request.form.get('q3')
-    # adds them to the database
-    dbhelpers.new_station_data(db, question1, question2, question3)
+    # makes sure the whole form is completed correctly
+    try: 
+        int(question3)
+    except:
+        flash('Please use only numbers for the temperature!')
+        return redirect(url_for('index'))
+    if question1 == 'Select here:' or question2 == 'Select here:' or question3 == '': 
+        flash('Please complete all fields correctly!')
+    else:
+        # adds them to the database
+        dbhelpers.new_station_data(db, question1, question2, question3)
+        flash('Station recorded successfully!')
     return redirect(url_for('index'))
 
 @app.route('/rankdata' , methods = ['GET','POST'])
 def rank():
     # gets all stations from the database, and calculates the score and ranks them
     stations = dbhelpers.get_stations(db)
-    return render_template('rankedlist.html', stations=stations)
+    stations_list = []
+    # ranks each station in the database
+    for station in stations:
+        name = station['q1']
+        geography = station['q2']
+        temperature = station['q3']
+        score_num = score(name, geography, temperature)
+        stations_list.append({'rank':None, 'q1':name, 'score':score_num})
+    sorted_stations = sorted(stations_list, key = lambda i: (i['score']))
+    for i in range(len(sorted_stations)):
+        sorted_stations[i]['rank'] = len(sorted_stations) - i
+    sorted_stations.reverse()
+    return render_template('rankedlist.html', stations = sorted_stations)
 
 @app.route('/resetlist', methods = ['POST'])
 def reset():
     # clears all stations from the database
     dbhelpers.reset_stations(db)
+    flash('Station data reset successfully!')
     return rank()
 
-
+@app.route('/moreinfo')
+def infopage():
+    return render_template('infopage.html')
 
 
 @app.template_filter('convert_time')
 def convert_time(ts):
-    """A jinja template helper to convert timestamps to timeago."""
+    # a jinja template helper to convert timestamps to timeago
     return timeago.format(ts, time.time())
 
 if __name__ == '__main__':
-    app.secret_key = 'mygroup'
+    app.secret_key = 'math4800' #used as the secret key for cookies
     app.config['SESSION_TYPE'] = 'filesystem'
     app.run(debug=True)
